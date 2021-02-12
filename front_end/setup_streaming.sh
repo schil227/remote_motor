ad423be3cdf590727ad31efc00ecd3aaef8a6db6
schil227 <schil227@morris.umn.edu>
2021-02-11 21:27:15 -0600

    setting up new server, made a few minor changes in fronted, including support for mobile and firefox

--- front_end/setup_streaming.sh
+++ front_end/setup_streaming.sh
@@ -1,11 +1,11 @@
-# /var/lib/streaming/setup_streaming.sh
+# /home/adrian/remote_motor_repo/remote_motor/front_end/setup_streaming.sh
 mkdir -p /dev/shm/streaming
 if [ ! -e /var/www/html/streaming ]; then
-        ln -s /dev/shm/treaming /var/www/html/streaming
+        ln -s /dev/shm/streaming /var/www/html/streaming
 fi
 if [ ! -e /var/www/html/streaming/index.html ]; then
-        ln -s /var/lib/streaming/index.html /var/www/html/index.html
+        ln -s /home/adrian/remote_motor_repo/remote_motor/front_end/index.html /var/www/html/index.html
 fi
 if [ ! -e /var/www/html/streaming/streaming.html ]; then
-        ln -s /var/lib/streaming/streaming.html /var/www/html/streaming/index.html
+        ln -s /home/adrian/remote_motor_repo/remote_motor/front_end/streaming.html /var/www/html/streaming/index.html
 fi
