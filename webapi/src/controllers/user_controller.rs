d21b9c2590fdd53b29cc23c6ea079f0a64dd3add
schil227 <schil227@morris.umn.edu>
2021-02-14 21:18:16 -0600

    Implemented commander count; heartbeat endpoint has been updated to look at the cookies, and the front end now has a 5 second heartbeat loop/display of current commander count.

--- webapi/src/controllers/user_controller.rs
+++ webapi/src/controllers/user_controller.rs
@@ -5,16 +5,17 @@ use std::sync::Mutex;
 
 use uuid::Uuid;
 
-use rocket::http::RawStr;
+use rocket::http::Cookies;
 use rocket::State;
 use rocket_contrib::json;
 
-#[post("/heartbeat/<user_id>", format = "application/json")]
-pub fn heartbeat(user_id: &RawStr, factory: State<Mutex<Factory>>) -> ApiResponse{
+#[post("/heartbeat", format = "application/json")]
+pub fn heartbeat(cookies: Cookies, factory: State<Mutex<Factory>>) -> ApiResponse{
+    let user_id = cookies.get("user_id").unwrap().value();
 
     let mut user_service = factory.lock().expect("Failed to obtain Factory!").user_service();
 
-    let user_count = user_service.heartbeat_user(Uuid::parse_str(user_id.as_str()).unwrap());
+    let user_count = user_service.heartbeat_user(Uuid::parse_str(user_id).unwrap());
         
     ApiResponse{
         json: json!({"status": "success", "user_count": user_count}),
